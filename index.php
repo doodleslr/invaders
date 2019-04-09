e4664c3a9a06b5ae2a56cffa7d45ff4387065d34
Daniel <dglarocca@hotmail.com>
2019-04-09 17:03:20 +1000

    Add files via upload
    changed index to php to help with local GET testing

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta name = "viewport" content = "user-scalable=no,width=device-width">
+    <link href = "main.css" rel = "stylesheet" type = "text/css">
+    <title>Daniel La Rocca</title>
+
+  </head>
+
+  <body>
+    <div id='container'>
+        <canvas></canvas>
+        <div id="My-Work">WORK</div>
+        <div id="Contact">CONTACT</div>
+        <div id="Skills">SKILLS</div>
+        <div id="About">ABOUT</div>
+        
+    </div>
+
+    <script type = "text/javascript" src="js/main.js">
+    </script>
+    <script type = "text/javascript" src="js/engine.js">
+    </script>
+    <script type = "text/javascript" src="js/display.js">
+    </script>
+    <script type = "text/javascript" src="js/controller.js">
+    </script>
+    <script type = "text/javascript" src="js/game.js">
+    </script>
+  </body>
+
+</html>
